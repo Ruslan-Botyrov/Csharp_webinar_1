16b38f80fb193ea6f0bfa48f7fa3facb3bf2d215
Ruslan <botyrovra@gmail.com>
2022-02-26 23:06:43 +0300

    add update homework 1 program

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -7,5 +7,5 @@ Console.Write("Введите число B");
 string inputNumberB = Console.ReadLine();
 int B = int.Parse(inputNumberB);
 
-if (A>B) Console.Write(A);
-else Console.Write(B);
+if (A>B) Console.Write($"Max = {A}");
+else Console.Write($"Max = {B}");
