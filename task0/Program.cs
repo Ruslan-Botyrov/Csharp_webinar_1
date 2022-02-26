765bae2e3f603785e592cdca71b23a10e2fb66cb
Ruslan <botyrovra@gmail.com>
2022-02-26 20:23:50 +0300

    add task0 programs

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число =>");  //вывод приглашения для ввода
+string strValue = Console.ReadLine();   //вывод строки
+double value = double.Parse(strValue);        //преобразуем строку в целое число
+
+double result = value * value;             //вычисление квадрата
+Console.WriteLine($"Квадрат числа {value} равен: {result}");              //вывод результата
