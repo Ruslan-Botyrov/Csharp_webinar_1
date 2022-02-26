cf91397871b22d7cb3c92e34ce40366d4703e36c
Ruslan <botyrovra@gmail.com>
2022-02-26 22:43:47 +0300

    add task 3 program

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// вычисление функции x = f(a)
+Console.Write("Введите число a");
+string inputNumber = Console.ReadLine();
+int numberA = int.Parse(inputNumber);
+
+int x = numberA +9;
+Console.WriteLine($"Функция x = f(a) равен: {x}");
