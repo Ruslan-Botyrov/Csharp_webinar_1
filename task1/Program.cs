16cced8f650a9494c0cab9ec7e1132bcd819be11
Ruslan <botyrovra@gmail.com>
2022-02-26 21:36:58 +0300

    add task1 program

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сравнение чисел
+Console.Write("Введите число a =>");
+string InputA = Console.ReadLine();
+int numberA = int.Parse(InputA);
+
+Console.Write("Введите число b =>");
+string InputB = Console.ReadLine();
+int numberB = int.Parse(InputB);
+
+int value = numberA * numberA;
+if (numberB == value) Console.WriteLine("Да");
+else
+{
+    Console.WriteLine("Нет");
+}
