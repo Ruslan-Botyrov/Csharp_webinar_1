4e2bb115cc596d6ff003fe0c2542492f138355f6
Ruslan <botyrovra@gmail.com>
2022-02-28 08:05:15 +0300

    update homework 4

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -3,10 +3,10 @@ Console.Write("Введите число");
 string inputNumber = Console.ReadLine();
 int N = int.Parse(inputNumber);
 
-int A = 1;
-Console.Write(A+" ");
-while (A < N)
+int A = 0;
+
+while (A < N-1)
 {
-    A = A + 1;
-Console.Write(A+" ");
+    A = A + 2;
+    Console.Write(A + " ");
 }
